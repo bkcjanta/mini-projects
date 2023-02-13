e0fb1a6d3c39ad946ef6695f703837a7140ef89e
bkcjanta <bhupendrafbook@gmail.com>
2023-02-13 12:57:21 +0530

    infinite scroll

---
+++ navbar/navbar.js
@@ -0,0 +1,12 @@
+export const navbar=`
+    <div id="navbar">
+        <div class="logo">log</div>
+        <div class="menu-item">
+            <a href="home.html">Home</a>
+            <a href="about.html">About</a>
+            <a href="dashboard.html">Dashboard</a>
+            <a href="login.html">Login</a>
+            <a href="signup.html">Signup</a>
+        </div>
+    </div>
+`
